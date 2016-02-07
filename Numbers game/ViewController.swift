923c14505ae80c234e4d5ff1a6701272ad57f750
Albero Nat <Ekolu@alberos-imac.local>
2016-02-07 19:08:08 +0100

    Initial Commit

---
+++ Numbers game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Numbers game
+//
+//  Created by Albero Nat on 2/7/16.
+//  Copyright © 2016 Kipras Sederevicius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
